b3a104286562c685395a08bd8df9e1d462111272
Johnathan Mowatt <jonmowatt@gmail.com>
2020-09-16 17:27:48 -0400

    Creating todo app

---
+++ todo/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <v-app>
+      <v-main>
+        <v-container>
+          <v-card class="mx-auto px-16" max-width="650">
+            <v-card-title class="justify-center">Todo App</v-card-title>
+
+            <!-- Search Field -->
+            <app-search></app-search>
+
+            <!-- List of Todos -->
+            <v-layout row>
+              <v-flex>
+                <v-list  class="px-0" v-for="item in 3" :key="item">
+                  <v-list-item>Testing</v-list-item>
+                </v-list>
+              </v-flex>
+            </v-layout>
+          </v-card>
+        </v-container>
+      </v-main>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import Search from "./components/Search.vue";
+
+export default {
+  components: {
+    "app-search": Search,
+  },
+
+  name: "app",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+body {
+  font-family: "Roboto";
+}
+</style>
---
+++ todo/src/components/Search.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <v-layout row>
+      <v-flex>
+        <v-text-field 
+          class="pr-4"
+          label="Search" 
+          v-model="search" 
+          solo>
+          </v-text-field>
+      </v-flex>
+      
+      <v-flex xs2>
+        <v-btn block class="blue white--text">
+          <v-icon small left>mdi-plus</v-icon>Add
+        </v-btn>
+      </v-flex>
+      
+    </v-layout>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      search: "",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
